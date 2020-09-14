0320ba2208de2d2116a655a8ebc9d6918ca31d5d
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-14 17:45:03 -0400

    created a query to get data from db and manipulate it in the console

--- db.js
+++ db.js
@@ -25,4 +25,14 @@ connection.connect((err, res) => {
 
 const runApp = (res) => {
   console.log(`App is running on id: ${connection.threadId}`);
+
+  connection.query(`SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name
+  FROM ((employee
+  INNER JOIN role ON employee.role_id = role.id)
+  INNER JOIN department ON role.department_id = department.id);`, (err, res) => {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log("Name: " + res[i].first_name + " " + res[i].last_name + " \nTitle: " + res[i].title);
+    }
+  })
 }
